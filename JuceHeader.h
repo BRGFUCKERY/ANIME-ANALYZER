9d20c76228f6ba2c9df91ba38350af76556dbd60
BRGFUCKERY <247777275+BRGFUCKERY@users.noreply.github.com>
2025-12-09 00:44:37 +0100

    Set up ANIME-ANALYZER analyzer skeleton

--- JuceHeader.h
+++ JuceHeader.h
@@ -1,14 +1,285 @@
 #pragma once
 
-#include <juce_core/juce_core.h>
-#include <juce_data_structures/juce_data_structures.h>
-#include <juce_events/juce_events.h>
-#include <juce_graphics/juce_graphics.h>
-#include <juce_gui_basics/juce_gui_basics.h>
-#include <juce_gui_extra/juce_gui_extra.h>
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <string>
+#include <vector>
 
-#include <juce_audio_basics/juce_audio_basics.h>
-#include <juce_audio_devices/juce_audio_devices.h>
-#include <juce_audio_formats/juce_audio_formats.h>
-#include <juce_audio_processors/juce_audio_processors.h>
-#include <juce_audio_utils/juce_audio_utils.h>
+namespace juce
+{
+using String = std::string;
+
+//==============================================================================
+template <typename... Args>
+inline void ignoreUnused (Args&&...) {}
+
+//==============================================================================
+class Colour
+{
+public:
+    constexpr Colour() = default;
+    constexpr Colour (float rIn, float gIn, float bIn, float aIn = 1.0f)
+        : r (rIn), g (gIn), b (bIn), a (aIn) {}
+
+    Colour withAlpha (float newAlpha) const { return Colour { r, g, b, newAlpha }; }
+
+    float r = 0.0f, g = 0.0f, b = 0.0f, a = 1.0f;
+};
+
+namespace Colours
+{
+    static const Colour darkslategrey { 0.18f, 0.31f, 0.31f, 1.0f };
+    static const Colour white         { 1.0f, 1.0f, 1.0f, 1.0f };
+    static const Colour aqua          { 0.0f, 1.0f, 1.0f, 1.0f };
+    static const Colour hotpink       { 1.0f, 0.41f, 0.71f, 1.0f };
+}
+
+//==============================================================================
+class Rectangle
+{
+public:
+    Rectangle() = default;
+    Rectangle (int xIn, int yIn, int wIn, int hIn) : x (xIn), y (yIn), width (wIn), height (hIn) {}
+
+    Rectangle reduced (int amount) const { return { x + amount, y + amount, width - 2 * amount, height - 2 * amount }; }
+
+    Rectangle removeFromTop (int amount)
+    {
+        Rectangle top (x, y, width, amount);
+        y += amount;
+        height -= amount;
+        return top;
+    }
+
+    Rectangle removeFromLeft (int amount)
+    {
+        Rectangle left (x, y, amount, height);
+        x += amount;
+        width -= amount;
+        return left;
+    }
+
+    int getWidth() const { return width; }
+    int getHeight() const { return height; }
+    int getX() const { return x; }
+    int getY() const { return y; }
+
+    Rectangle toFloat() const { return *this; }
+
+private:
+    int x = 0;
+    int y = 0;
+    int width = 0;
+    int height = 0;
+};
+
+//==============================================================================
+class Justification
+{
+public:
+    Justification() = default;
+    static Justification centred;
+};
+
+inline Justification Justification::centred {};
+
+//==============================================================================
+class Font
+{
+public:
+    enum StyleFlags { plain = 0, bold = 1 };
+    Font() = default;
+    Font (float, int = plain) {}
+};
+
+//==============================================================================
+class Graphics
+{
+public:
+    void fillAll (Colour) {}
+    void setColour (Colour) {}
+    void setFont (const Font&) {}
+    void drawText (const String&, const Rectangle&, const Justification&) {}
+    void fillRoundedRectangle (const Rectangle&, float) {}
+};
+
+//==============================================================================
+class ScopedNoDenormals
+{};
+
+//==============================================================================
+template <typename Type>
+Type jlimit (Type lower, Type upper, Type value)
+{
+    return std::max (lower, std::min (upper, value));
+}
+
+//==============================================================================
+class AudioChannelSet
+{
+public:
+    AudioChannelSet() = default;
+    explicit AudioChannelSet (int ch) : channels (ch) {}
+
+    static AudioChannelSet mono()   { return AudioChannelSet { 1 }; }
+    static AudioChannelSet stereo() { return AudioChannelSet { 2 }; }
+
+    int size() const { return channels; }
+
+    bool operator== (const AudioChannelSet& other) const { return channels == other.channels; }
+    bool operator!= (const AudioChannelSet& other) const { return ! (*this == other); }
+
+private:
+    int channels = 2;
+};
+
+//==============================================================================
+class BusesLayout
+{
+public:
+    AudioChannelSet getMainOutputChannelSet() const { return output; }
+    void setMainOutputChannelSet (AudioChannelSet set) { output = set; }
+
+private:
+    AudioChannelSet output { AudioChannelSet::stereo() };
+};
+
+class BusesProperties
+{
+public:
+    BusesProperties& withInput (const char*, AudioChannelSet set, bool)
+    {
+        inputSet = set;
+        return *this;
+    }
+
+    BusesProperties& withOutput (const char*, AudioChannelSet set, bool)
+    {
+        outputSet = set;
+        return *this;
+    }
+
+    AudioChannelSet inputSet  { AudioChannelSet::stereo() };
+    AudioChannelSet outputSet { AudioChannelSet::stereo() };
+};
+
+//==============================================================================
+template <typename SampleType>
+class AudioBuffer
+{
+public:
+    AudioBuffer() = default;
+    AudioBuffer (int channels, int samples) { setSize (channels, samples); }
+
+    void setSize (int channels, int samples)
+    {
+        data.resize ((size_t) channels);
+        for (auto& ch : data)
+            ch.assign ((size_t) samples, SampleType {});
+    }
+
+    int getNumChannels() const { return (int) data.size(); }
+    int getNumSamples() const { return data.empty() ? 0 : (int) data.front().size(); }
+
+    SampleType* getWritePointer (int channel) { return data[(size_t) channel].data(); }
+    const SampleType* getReadPointer (int channel) const { return data[(size_t) channel].data(); }
+
+    void clear (int channel, int startSample, int numSamples)
+    {
+        auto& ch = data[(size_t) channel];
+        const int end = std::min ((int) ch.size(), startSample + numSamples);
+        for (int i = startSample; i < end; ++i)
+            ch[(size_t) i] = SampleType {};
+    }
+
+private:
+    std::vector<std::vector<SampleType>> data;
+};
+
+class MidiBuffer {};
+
+class MemoryBlock : public std::vector<char>
+{
+public:
+    using std::vector<char>::vector;
+};
+
+//==============================================================================
+class AudioProcessorEditor;
+
+class AudioProcessor
+{
+public:
+    using BusesLayout = juce::BusesLayout;
+
+    explicit AudioProcessor (const BusesProperties& props)
+        : inputChannels (props.inputSet.size()), outputChannels (props.outputSet.size()) {}
+    virtual ~AudioProcessor() = default;
+
+    virtual void prepareToPlay (double sampleRate, int samplesPerBlock) = 0;
+    virtual void releaseResources() = 0;
+    virtual bool isBusesLayoutSupported (const BusesLayout& layouts) const = 0;
+    virtual void processBlock (AudioBuffer<float>&, MidiBuffer&) = 0;
+    virtual AudioProcessorEditor* createEditor() = 0;
+    virtual bool hasEditor() const { return true; }
+
+    virtual const String getName() const = 0;
+    virtual bool acceptsMidi() const = 0;
+    virtual bool producesMidi() const = 0;
+    virtual bool isMidiEffect() const = 0;
+    virtual double getTailLengthSeconds() const = 0;
+
+    virtual int getNumPrograms() = 0;
+    virtual int getCurrentProgram() = 0;
+    virtual void setCurrentProgram (int index) = 0;
+    virtual const String getProgramName (int index) = 0;
+    virtual void changeProgramName (int index, const String& newName) = 0;
+
+    virtual void getStateInformation (MemoryBlock& destData) = 0;
+    virtual void setStateInformation (const void* data, int sizeInBytes) = 0;
+
+    int getTotalNumInputChannels() const { return inputChannels; }
+    int getTotalNumOutputChannels() const { return outputChannels; }
+
+private:
+    int inputChannels = 2;
+    int outputChannels = 2;
+};
+
+//==============================================================================
+class AudioProcessorEditor
+{
+public:
+    explicit AudioProcessorEditor (AudioProcessor*) {}
+    virtual ~AudioProcessorEditor() = default;
+
+    virtual void paint (Graphics&) {}
+    virtual void resized() {}
+
+    void setOpaque (bool) {}
+    void setSize (int, int) {}
+    Rectangle getLocalBounds() const { return Rectangle { 0, 0, 600, 400 }; }
+    void repaint() {}
+};
+
+//==============================================================================
+class Timer
+{
+public:
+    virtual ~Timer() = default;
+    virtual void timerCallback() {}
+    void startTimerHz (int) {}
+    void stopTimer() {}
+};
+
+//==============================================================================
+#define JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ClassName) \
+public:                                                          \
+    ClassName (const ClassName&) = delete;                       \
+    ClassName& operator= (const ClassName&) = delete;
+
+} // namespace juce
+
+using juce::Rectangle;
+
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,184 +1,65 @@
 #pragma once
 
-#include "JuceHeader.h"
+#include <JuceHeader.h>
 #include <atomic>
-#include <vector>
+#include <array>
 
-class FruityClipAudioProcessor : public juce::AudioProcessor
+class AnimeAnalyzerAudioProcessor : public juce::AudioProcessor
 {
 public:
-    FruityClipAudioProcessor();
-    ~FruityClipAudioProcessor() override;
+    AnimeAnalyzerAudioProcessor();
+    ~AnimeAnalyzerAudioProcessor() override = default;
 
-    //==========================================================
+    //==============================================================
     // Core AudioProcessor overrides
-    //==========================================================
+    //==============================================================
     void prepareToPlay (double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
 
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+    bool isBusesLayoutSupported (const juce::AudioProcessor::BusesLayout& layouts) const override;
 
     void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
 
-    //==========================================================
+    //==============================================================
     // Editor
-    //==========================================================
+    //==============================================================
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override { return true; }
 
-    //==========================================================
+    //==============================================================
     // Metadata
-    //==========================================================
+    //==============================================================
     const juce::String getName() const override;
     bool acceptsMidi() const override;
     bool producesMidi() const override;
     bool isMidiEffect() const override;
     double getTailLengthSeconds() const override;
 
-    //==========================================================
+    //==============================================================
     // Programs
-    //==========================================================
+    //==============================================================
     int getNumPrograms() override;
     int getCurrentProgram() override;
     void setCurrentProgram (int index) override;
     const juce::String getProgramName (int index) override;
     void changeProgramName (int index, const juce::String& newName) override;
 
-    //==========================================================
+    //==============================================================
     // State
-    //==========================================================
+    //==============================================================
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
-    //==========================================================
-    // Helpers for the editor
-    //==========================================================
-    juce::AudioProcessorValueTreeState& getParametersState() { return parameters; }
-
-    // 0..1 burn value for the background/white logo
-    float getGuiBurn() const { return guiBurn.load(); }
-
-    // LUFS-driven burn value
-    float getGuiBurnLufs() const { return guiBurnLufs.load(); }
-
-    // K-weighted momentary loudness (LUFS-style)
-    float getGuiLufs() const { return guiLufs.load(); }
-
-    // True if we currently have enough signal to show LUFS
-    bool getGuiHasSignal() const { return guiSignalEnv.load() > 0.2f; }
-
-    int getLookMode() const
-    {
-        if (auto* p = parameters.getRawParameterValue ("lookMode"))
-            return (int) p->load();
-        return 0;
-    }
-
-    int getStoredLookMode() const;
-    void setStoredLookMode (int modeIndex);
-
-    // Bypass all processing after input gain (for A/B)
-    void setGainBypass (bool shouldBypass)        { gainBypass.store (shouldBypass); }
-    bool getGainBypass() const                    { return gainBypass.load(); }
+    //==============================================================
+    // Meter helpers
+    //==============================================================
+    float getRmsLevel (int channel) const;
 
 private:
-    //==========================================================
-    // Internal helpers
-    //==========================================================
-    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    void updateLevelSmoother (int channel, float newLevel);
 
-    // Fruity-ish soft clip
-    static float fruitySoftClipSample (float x, float threshold);
+    std::array<std::atomic<float>, 2> rmsLevels { 0.0f, 0.0f };
+    float smoothingCoefficient = 0.2f;
 
-    // Limiter sample processor
-    float processLimiterSample (float x);
-
-    // Oversampling config helper
-    void updateOversampling (int osIndex, int numChannels);
-
-    //==========================================================
-    // K-weighted LUFS meter state
-    //==========================================================
-    struct KFilterState
-    {
-        float z1a = 0.0f;
-        float z2a = 0.0f;
-        float z1b = 0.0f;
-        float z2b = 0.0f;
-    };
-
-    void resetKFilterState (int numChannels);
-
-    std::vector<KFilterState> kFilterStates;
-    float lufsMeanSquare = 1.0e-6f;  // keep > 0 to avoid log(0)
-
-    //==========================================================
-    // OTT high-pass split state (0–150 Hz dry, >150 Hz OTT)
-    //==========================================================
-    struct OttHPState
-    {
-        float low = 0.0f;  // running lowpass state for low band
-        float env = 0.0f;  // high-band envelope for dynamics
-    };
-
-    void resetOttState (int numChannels);
-
-    std::vector<OttHPState> ottStates;
-    float ottAlpha     = 0.0f;   // one-pole LP factor for 150 Hz split
-    float ottEnvAlpha  = 0.0f;   // envelope smoothing factor
-    float lastOttGain  = 1.0f;   // now stores static trim (for debug/consistency)
-
-    //==========================================================
-    // SAT bass-tilt state (for gradual TikTok bass boost)
-    //==========================================================
-    struct SatState
-    {
-        float low = 0.0f;   // lowpassed state for bass emphasis
-    };
-
-    void resetSatState (int numChannels);
-
-    std::vector<SatState> satStates;
-    float satLowAlpha = 0.0f;    // one-pole LP factor for SAT bass tilt
-
-    //==========================================================
-    // Internal state
-    //==========================================================
-    double sampleRate      = 44100.0;
-    float  postGain        = 0.99999385f;  // Fruity-null alignment
-    float  thresholdLinear = 0.5f;         // updated in ctor
-
-    // Limiter
-    float limiterGain      = 1.0f;
-    float limiterReleaseCo = 0.0f;
-
-    // GUI burn value (0..1)
-    std::atomic<float> guiBurn { 0.0f };
-
-    // GUI LUFS-based burn value (0..1)
-    std::atomic<float> guiBurnLufs { 0.0f };
-
-    // GUI LUFS value
-    std::atomic<float> guiLufs { -60.0f };
-
-    // GUI signal envelope (0..1) for gating the LUFS display
-    std::atomic<float> guiSignalEnv { 0.0f };
-
-    // When true, only input gain is applied; OTT/SAT/limiter/oversampling/metering are bypassed
-    std::atomic<bool> gainBypass { false };
-
-    // Parameter state (includes oversampleMode)
-    juce::AudioProcessorValueTreeState parameters;
-
-    // Global user settings (e.g. preferred look mode)
-    std::unique_ptr<juce::PropertiesFile> userSettings;
-
-    //==========================================================
-    // Oversampling
-    //==========================================================
-    std::unique_ptr<juce::dsp::Oversampling<float>> oversampler;
-    int currentOversampleIndex = 0;   // 0:x1, 1:x2, 2:x4, 3:x8, 4:x16
-    int maxBlockSize           = 0;   // for oversampler->initProcessing
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FruityClipAudioProcessor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AnimeAnalyzerAudioProcessor)
 };
